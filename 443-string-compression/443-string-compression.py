d2dd22a387a49ce62466ea89a636448bbcb1548c
ZerubabelK <95906335+ZerubabelK@users.noreply.github.com>
2022-02-27 16:43:40 +0300

    Time: 102 ms (28.48%), Space: 14.1 MB (69.71%) - LeetHub

--- 443-string-compression/443-string-compression.py
+++ 443-string-compression/443-string-compression.py
@@ -2,18 +2,19 @@ class Solution:
     def compress(self, chars: List[str]) -> int:
         s = ""
         t = chars[0]
-        a = 1
+        j = 1
         for i in chars[1:]:
+            print(s)
             if i == t:
-                a += 1
+                j += 1
             else:
                 s += t
                 t = i
-                if a != 1:
-                    s+=str(a)
-                a = 1
+                if j != 1:
+                    s+=str(j)
+                j = 1
         s += t
-        if a != 1:
-            s+=str(a)
+        if j != 1:
+            s+=str(j)
         chars[:] = [i for i in s]
         return len(s)
