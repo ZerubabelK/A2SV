b28a511b8ed60d456d27f5e8731ef400fc4d2df4
ZerubabelK <95906335+ZerubabelK@users.noreply.github.com>
2022-03-01 11:46:01 +0300

    Time: 68 ms (12.43%), Space: 14.1 MB (46.82%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -8,17 +8,14 @@ class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         left = root.left
         right = root.right
-        # while left and right:
-        #     if left.val != right.val:
-        #         return False
-        #     left = left.left
-        #     right = right.right
-        # return True     
+           
         def symmetry(left, right):
             if left == None and right != None:
                 return False
+            
             if left != None and right == None:
                 return False
+            
             if left == None and right == None:
                 return True
             
@@ -26,4 +23,5 @@ class Solution:
                 return symmetry(left.left, right.right) and symmetry(left.right, right.left)
             else:
                 return False
+            
         return symmetry(left, right)
