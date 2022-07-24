188ffe629a24c7351db2bb4f2edaf9e785412b97
Zerubabel Kassahun <95906335+ZerubabelK@users.noreply.github.com>
2022-07-24 08:12:58 -0700

    Time: 314 ms (25.96%), Space: 20.5 MB (13.29%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -1,19 +1,14 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        def dfs(row, col):
-            nonlocal _ans
-            _ans = matrix[row][col] == target or _ans
-            visited.add((row, col))
-            for d in DIR:
-                n_row, n_col = row + d[0], col + d[1]
-                if (n_row, n_col) in visited or not inbound(n_row, n_col):
-                    continue
-                dfs(n_row, n_col)
-        
-        m, n = len(matrix), len(matrix[0])
-        DIR = [(0,1),(1,0),(-1,0),(0,-1)]
-        inbound = lambda row, col: 0 <= row < m and 0 <= col < n
-        visited = set()
-        _ans = False
-        dfs(0, 0)
-        return _ans
+        for row in matrix:
+            left = 0
+            right = len(row) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if row[mid] > target:
+                    right = mid - 1
+                elif row[mid] < target:
+                    left = mid + 1
+                else:
+                    return True
+        return False
