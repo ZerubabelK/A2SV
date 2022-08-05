cc5eacd4c7284b60228bca61fcd75462ba4b3687
Zerubabel Kassahun <95906335+ZerubabelK@users.noreply.github.com>
2022-08-05 07:44:03 +0300

    Time: 7091 ms (11.52%), Space: 18.3 MB (5.65%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        _max = 1
+        memo = {}
+        
+        def dp(i):
+            depth = 0
+
+            if i in memo:
+                return memo[i]
+            
+            for j in range(i+1, len(nums)):
+                if nums[j] > nums[i]:
+                    depth = max(dp(j), depth)
+            memo[i] = depth + 1
+            return depth + 1
+        
+        for i in range(len(nums)):
+            _max = max(dp(i), _max)
+        return _max
+            
+            
