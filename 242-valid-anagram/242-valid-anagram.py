b5c85bb0c25370ea9b59563ad856c74dbdb642cd
Zerubabel Kassahun <95906335+ZerubabelK@users.noreply.github.com>
2022-08-10 06:52:22 -0700

    Time: 58 ms (80.71%), Space: 14.4 MB (97.07%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,8 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        _freq = defaultdict(int)
-        for l in s:
-            _freq[l] += 1
+        _freq = Counter(s)
         
         for l in t:
             if l not in _freq:
@@ -12,5 +10,6 @@ class Solution:
         for key in _freq:
             if _freq[key] != 0:
                 return False
-            
+        
         return True
+        
