6078c37a8c096e4d0e705dc720881d9c09c95992
Zerubabel Kassahun <95906335+ZerubabelK@users.noreply.github.com>
2022-08-12 05:08:10 -0700

    Time: 700 ms (91.08%), Space: 14 MB (60.50%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def helper(l, r):
+            nonlocal res
+            nonlocal resLen
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+                
+        res = ""
+        resLen = 0
+
+        for i in range(len(s)):
+            l, r = i, i
+            helper(l, r)
+            
+            l, r = i, i+1
+            helper(l, r)
+            
+        return res
