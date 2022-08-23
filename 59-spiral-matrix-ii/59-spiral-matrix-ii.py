88200f16579c537c32a6069387a1933d6e42d0cc
Zerubabel Kassahun <95906335+ZerubabelK@users.noreply.github.com>
2022-08-23 01:58:41 -0700

    Time: 45 ms (65.71%), Space: 13.9 MB (83.07%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.py
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -2,17 +2,15 @@ class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
         ans = [[0 for i in range(n)] for j in range(n)]
         DIR = [(0,1), (1,0), (0,-1), (-1,0)]
-        visited = set()
+        # visited = set()
         i, j, d = 0, 0, 0
         inbound = lambda row, col: 0 <= row < n and 0 <= col < n
         val = 1
-        while len(visited) < n * n:
-            if (i,j) not in visited:
+        while val <= n ** 2:
+            if ans[i][j] == 0:
                 ans[i][j] = val
                 val += 1
-            visited.add((i,j))
-
-            if inbound(i + DIR[d][0],j + DIR[d][1]) and (i + DIR[d][0],j + DIR[d][1]) not in visited:
+            if inbound(i + DIR[d][0], j + DIR[d][1]) and ans[i + DIR[d][0]][j + DIR[d][1]] == 0:
                 i += DIR[d][0]
                 j += DIR[d][1]
             
@@ -21,6 +19,7 @@ class Solution:
                     d += 1
                     i += DIR[d][0]
                     j += DIR[d][1]
+                    
                 else:
                     d = 0
                     i += DIR[d][0]
