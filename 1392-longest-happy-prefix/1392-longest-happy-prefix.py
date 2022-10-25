b966f30e42d48f833a958819116c0d67ee50e726
Zerubabel Kassahun <95906335+ZerubabelK@users.noreply.github.com>
2022-10-25 05:08:00 -0700

    Time: 324 ms (86.22%), Space: 19 MB (31.50%) - LeetHub

--- 1392-longest-happy-prefix/1392-longest-happy-prefix.py
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.py
@@ -1,23 +1,20 @@
 class Solution:
     def longestPrefix(self, s: str) -> str:
-        length = self.computeLPSArray(s, len(s))[-1]
+        length = self.findLPS(s)[-1]
         return s[:length]
     
-    def computeLPSArray(self, pat, M):
-        j = 0
-
-        lps = [0] * M
-        i = 1
-
-        while i < M:
-            if pat[i]== pat[j]:
-                j += 1
-                lps[i] = j
+    def findLPS(self, s):
+        n = len(s)
+        i, j = 0, 1
+        LPS = [0] * n
+        while j < n:
+            if s[i] == s[j]:
+                LPS[j] = i + 1
                 i += 1
+                j += 1
+            elif i == 0: 
+                LPS[j] = 0
+                j += 1
             else:
-                if j != 0:
-                    j = lps[j-1]
-                else:
-                    lps[i] = 0
-                    i += 1
-        return lps
+                i = LPS[i-1]
+        return LPS
