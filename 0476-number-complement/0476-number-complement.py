a79a0342245fc0545c1abfa17d62862ce7d507d6
Zerubabel Kassahun <95906335+ZerubabelK@users.noreply.github.com>
2022-11-02 04:18:46 -0700

    Time: 32 ms (93.18%), Space: 13.8 MB (53.45%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -4,7 +4,7 @@ class Solution:
         complement = 0
         while num != 0:
             bit = num & 1
-            complement += (2 ** power) * (1 if bit == 0 else 0)
+            complement += (1 << power) * (1 if bit == 0 else 0)
             power += 1
             num = num >> 1
         return complement
