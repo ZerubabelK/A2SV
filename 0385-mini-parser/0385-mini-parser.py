f9524ad9555750994940070c53d3cd3e20dc9a0d
Zerubabel Kassahun <95906335+ZerubabelK@users.noreply.github.com>
2023-02-21 23:49:17 +0300

    Time: 112 ms (18.24%), Space: 23.8 MB (14.71%) - LeetHub

---
+++ 0385-mini-parser/0385-mini-parser.py
@@ -0,0 +1,56 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def deserialize_core(self, data_list):
+        if isinstance(data_list, list):
+            nested_integer = NestedInteger()
+            for n in data_list:
+                nested_integer.add(self.deserialize_core(n))
+            return nested_integer
+        else:
+            return NestedInteger(data_list)
+    
+    def deserialize(self, s):
+        data = eval(s)
+        return self.deserialize_core(data)
