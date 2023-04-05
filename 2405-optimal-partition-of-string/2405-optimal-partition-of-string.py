780c91f840253879c420c0508a6e3905b8afcd6c
Zerubabel Kassahun <95906335+ZerubabelK@users.noreply.github.com>
2023-04-05 09:46:28 +0300

    Time: 279 ms (9.98%), Space: 14.6 MB (90.81%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -1,11 +1,12 @@
 class Solution:
     def partitionString(self, s: str) -> int:
-        visited = set()
+        visited = 0
         count = 0
+        isVisited = lambda ch, visited: visited & 1 << (ord(ch) - ord('a'))
         for ch in s:
-            if ch in visited:
+            if isVisited(ch, visited):
                 count += 1
-                visited.clear()
-            visited.add(ch)
+                visited = 0
+            visited = visited ^ 1 << (ord(ch) - ord('a'))
         return count + (1 if visited else 0)
                 
