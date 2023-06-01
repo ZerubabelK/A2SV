6db8bbc170367739ad171d611dcb2020ae4b92e0
Zerubabel Kassahun <95906335+ZerubabelK@users.noreply.github.com>
2023-06-01 19:47:56 +0300

    Time: 46 ms (40.36%), Space: 16.4 MB (18.67%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -5,14 +5,10 @@ class Solution:
         return max(self.find(nums[:len(nums)-1]), self.find(nums[1:]))
     
     def find(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        if len(nums) <= 2: 
-            return max(nums)
-        
-        robbed=[nums[0], max(nums[0],nums[1])]
-        
-        for i in range(2,len(nums)):
-            robbed.append(max(nums[i]+robbed[-2],robbed[-1]))
-            
-        return robbed[-1]
+        prev1 = 0
+        prev2 = 0
+        for num in nums:
+            dp = max(prev1, prev2 + num)
+            prev2 = prev1
+            prev1 = dp  
+        return prev1
