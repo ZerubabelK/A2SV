a42ce67b12fb17db91bd0bf77d318a62c184c50c
Zerubabel Kassahun <95906335+ZerubabelK@users.noreply.github.com>
2023-06-01 18:10:26 +0300

    Time: 5609 ms (20.82%), Space: 34.8 MB (5.01%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,15 +1,13 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        def dp(i, memo = {}):
+        @cache
+        def dp(i):
             if i == len(nums)-1:
                 return 0
             if i >= len(nums):
                 return float('inf')
-            if i in memo:
-                return memo[i]
             temp = float('inf')
             for j in range(i+1, i + nums[i] + 1):
-                temp = min(temp, dp(j, memo))
-            memo[i] = temp + 1
-            return memo[i]
+                temp = min(temp, dp(j))
+            return temp + 1
         return dp(0)
